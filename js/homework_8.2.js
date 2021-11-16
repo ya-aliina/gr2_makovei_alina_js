40d26d438132aa64229651da2b8d85f3c0468402
Alina Makovey <93222569+ya-aliina@users.noreply.github.com>
2021-11-16 12:46:26 +0200

    Add files via upload

---
+++ js/homework_8.2.js
@@ -0,0 +1,43 @@
+/*------------------------------Задание №2----------------------------------------
+Написать код, который создаст на странице 2 прямоугольника разного цвета и размера.
+При наведении курсора мыши на прямоугольник вывести в консоль размер этого
+прямоугольника.
+При клике на прямоугольник - выдать уведомление, в котором будет русское название
+цвета этого прямоугольника.
+---------------------------------------------------------------------------------*/
+
+document.body.style.display = 'flex';
+
+//--------------Первый прямоугольник--------------
+let firstRectangle = document.createElement('div');
+firstRectangle.setAttribute('rusСolor', 'розовый');
+document.body.prepend(firstRectangle);
+
+firstRectangle.style.height = '150px';
+firstRectangle.style.width = '300px';
+firstRectangle.style.background = 'pink';
+firstRectangle.style.marginRight = '50px'
+
+//--------------Второй прямоугольник--------------
+let secondRectangle = document.createElement('div');
+secondRectangle.setAttribute('rusСolor', 'бирюзовый');
+document.body.append(secondRectangle);
+
+secondRectangle.style.height = '100px';
+secondRectangle.style.width = '200px';
+secondRectangle.style.background = '#30d5c8';
+
+//-------------------События------------------------
+let rectangles = document.getElementsByTagName('div');
+
+for (let i = 0; i < rectangles.length; i++ ) {
+    let rectangle = rectangles[i];
+
+    rectangle.onmouseover = function () {
+        console.log( `${this.style.height}, ${this.style.width}` );
+    }
+
+    rectangle.onclick = function () {
+        alert( this.getAttribute('rusСolor') );
+    }
+}
---
+++ js/homework_9.2.js
@@ -0,0 +1,101 @@
+/*--------------------------Задачие №2------------------------------------
+Написать код, который будет выполнять 5 действия на странице index.html:
++1. Создать 100 квадратов голубого цвета размером 70*70 пикселей, и
+распологает их вертикально.
++2. Изменить цвет каждого квадрата на фиолетовый, и изменить размер 100*100
+пикселей.
++3. Вывести на каждом квадрате его порядковый номер.
++4. Изменить цвет каждого 3-го квадрата на красный, каждый 15 квадрат сделать
+треугольником цветом хаки и любые 5 сделать желтым.
++5. Удалить все красные квадраты.
+Каждое действие должно выполниться через 2 секунды от предыдущего, первое
+действие также должно иметь задержку 2 секунды.
+ -----------------------------------------------------------------------*/
+$(document).ready(function() {
+
+    const MS_SECOND = 1000;
+    let numberOfSquares = 100;
+
+    function createBlueSquares (numberOfSquares) {
+        for (let i = 0; i < numberOfSquares; i++) {
+            $('body').append('<div></div>');
+        }
+
+        $('div').css({
+            'height' : '70px',
+            'width' : '70px',
+            'background' : '#87ceeb',
+            'margin': '0 auto',
+            'margin-bottom' : '10px',
+        });
+        return true;
+    }
+
+    function  changeToBiggerAndPurple () {
+        $('div').css({
+            'height' : '100px',
+            'width' : '100px',
+            'background' : '#ab99b8',
+            'margin': '0 auto',
+            'margin-bottom' : '10px',
+        });
+        return true;
+    }
+
+    function  addNumber () {
+        $('div').text(function(index, text) {
+            text = index + 1;
+            return text;
+        });
+
+        $('div').css({
+            'color' : 'white',
+            'display' : 'flex',
+            'justify-content' : 'center',
+            'align-items' : 'center',
+        });
+        return true;
+    }
+
+    function changeColorAndAddTriangle () {
+        let actions = 0;
+
+        $('div').each(function (index, element) {
+
+            if ( (index + 1) % 3 === 0 && (index + 1) % 15 !== 0 ) {
+                $(element).css({
+                    'background' : 'red',
+                });
+                $(element).attr('class', 'delete');
+            }
+            if ( (index + 1) % 15 === 0 ) {
+                $(element).css({
+                    'background' : 'linear-gradient(to right bottom, transparent 48%, rgb(114, 134, 57) 50%) left / 50.25% 100% no-repeat, linear-gradient(to left bottom, transparent 48%, rgb(114, 134, 57) 50%) right / 50.25% 100% no-repeat',
+                });
+            }
+            if ( (index - 1) % 3 === 0 && actions < 5) {
+                $(element).css({
+                    'background' : 'yellow',
+                    'color' : 'black',
+                });
+                actions ++;
+            }
+        });
+        return true;
+    }
+
+    function deleteRedSquares () {
+        $('div').each(function (index, element) {
+
+            if  ($(element).css('background-color') === 'rgb(255, 0, 0)') {
+                $(element).remove();
+            }
+        });
+    }
+
+    setTimeout(createBlueSquares, 2 * MS_SECOND, numberOfSquares);
+    setTimeout(changeToBiggerAndPurple, 4 * MS_SECOND);
+    setTimeout(addNumber, 6 * MS_SECOND );
+    setTimeout(changeColorAndAddTriangle, 8 * MS_SECOND );
+    setTimeout(deleteRedSquares, 10 * MS_SECOND );
+});
