2dd7046a8cb6326621ac650e600c046c7fd062e1
Alina Makovey <93222569+ya-aliina@users.noreply.github.com>
2021-11-16 15:10:47 +0200

    Update homework_1.js

--- js/homework_1.js
+++ js/homework_1.js
@@ -1,49 +1,51 @@
+const NAME = 'Алина';
+const PATRONYMIC = 'Сергеевна';
+const DATE_OF_BIRTH = '22 апреля 1999г';
+const HEIGHT = 1.68;
+
 //Приветствие
-let greetings = 'Добро пожаловать!'
-alert (greetings)
+let greetings = 'Добро пожаловать!';
+
 //Обо мне
-let surname = 'Маковей'
-const NAME = 'Алина'
-const PATRONYMIC = 'Сергеевна'
-const DATE_OF_BIRTH = '22 апреля 1999г'
-let age = 22
-let relationship_status = 'Не замужем'
-let higher_education = 'Магистр'
-let university = 'Национальный аэрокосмический университет имени Н. Е. Жуковского "ХАИ"'
-let speciality = 'Менеджмент'
-let specialization = 'Управление проектами'
-let native_language = 'Русский'
-let learning_language = 'Английский'
-let children = 'Нет'
-const HEIGHT = 1.68
-let hair_color = 'Каштановый'
-const EYE_COLOR = 'Зеленый'
-let favorite_color = 'Фиолетовый'
-let favorite_genre_of_music = 'Поп'
-let favorite_artist = 'Dua Lipa'
-let favorite_movie_genre = 'Фэнтези'
-let favourite_tv_show = 'Офис'
-let pets = 'Кот'
-//Вывод в консоль
-console.log('Фамилия: '+surname)
-console.log('Имя: '+NAME)
-console.log('Отчество: '+PATRONYMIC)
-console.log('Дата рождения: '+DATE_OF_BIRTH)
-console.log('Возраст: '+age)
-console.log('Семейное положение: '+relationship_status)
-console.log('Высшее образование: '+higher_education)
-console.log('Университет: '+university)
-console.log('Специальность: '+speciality)
-console.log('Специализация: '+specialization)
-console.log('Родной язык: '+native_language)
-console.log('Изучаемый язык: '+learning_language)
-console.log('Дети: '+children)
-console.log('Рост: '+HEIGHT)
-console.log('Цвет волос: '+hair_color)
-console.log('Цвет глаз: '+EYE_COLOR)
-console.log('Любымый цвет: '+favorite_color)
-console.log('Любымый жанр музыки: '+favorite_genre_of_music)
-console.log('Любимый исполнитель: '+favorite_artist)
-console.log('Любимый жанр кино: '+favorite_movie_genre)
-console.log('Любимый сериал: '+favourite_tv_show)
-console.log('Домашние животные '+pets)
+let surname = 'Маковей';
+let age = 22;
+let relationship_status = 'Не замужем';
+let higher_education = 'Магистр';
+let university = 'Национальный аэрокосмический университет имени Н. Е. Жуковского "ХАИ"';
+let speciality = 'Менеджмент';
+let specialization = 'Управление проектами';
+let native_language = 'Русский';
+let learning_language = 'Английский';
+let children = 'Нет';
+let hair_color = 'Каштановый';
+let favorite_color = 'Фиолетовый';
+let favorite_genre_of_music = 'Поп';
+let favorite_artist = 'Dua Lipa';
+let favorite_movie_genre = 'Фэнтези';
+let favourite_tv_show = 'Офис';
+let pets = 'Кот';
+
+//Вывод информации
+alert (greetings);
+console.log('Фамилия: '+surname);
+console.log('Имя: '+NAME);
+console.log('Отчество: '+PATRONYMIC);
+console.log('Дата рождения: '+DATE_OF_BIRTH);
+console.log('Возраст: '+age);
+console.log('Семейное положение: '+relationship_status);
+console.log('Высшее образование: '+higher_education);
+console.log('Университет: '+university);
+console.log('Специальность: '+speciality);
+console.log('Специализация: '+specialization);
+console.log('Родной язык: '+native_language);
+console.log('Изучаемый язык: '+learning_language);
+console.log('Дети: '+children);
+console.log('Рост: '+HEIGHT);
+console.log('Цвет волос: '+hair_color);
+console.log('Цвет глаз: '+EYE_COLOR);
+console.log('Любымый цвет: '+favorite_color);
+console.log('Любымый жанр музыки: '+favorite_genre_of_music);
+console.log('Любимый исполнитель: '+favorite_artist);
+console.log('Любимый жанр кино: '+favorite_movie_genre);
+console.log('Любимый сериал: '+favourite_tv_show);
+console.log('Домашние животные '+pets);
