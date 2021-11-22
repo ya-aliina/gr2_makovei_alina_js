fd6d0ed33a63856b2a043dd435631ccbd4c41268
Alina Makovey <93222569+ya-aliina@users.noreply.github.com>
2021-11-22 05:21:58 +0200

    Update homework_5_6.js

--- js/homework_5_6.js
+++ js/homework_5_6.js
@@ -1,1 +1,25 @@
+/*--------------------------------------Задача №6--------------------------------------------
+Напишите функцию truncate(str, maxlength), которая проверяет длину строки str, и если она
+превосходит maxlength – заменяет конец str на "...", так чтобы ее длина стала равна maxlength.
+--------------------------------------------------------------------------------------------*/
+function truncate(str, maxlength) {
+    if (typeof str !== 'string') {
+        console.error('Это не строка!');
+        return null;
+    }
 
+    if (str.length > maxlength) {
+        let symbols = '...';
+        let maxlength_without_symbols = maxlength - symbols.length;
+
+        let short_str = str.slice(0, maxlength_without_symbols);
+
+        let result = (`${short_str}${symbols}`);
+        return result;
+    }
+}
+
+let str = 'Гремел гром и ярко сверкала молния';
+let max_length = 21;
+
+console.log(truncate(str, max_length));
