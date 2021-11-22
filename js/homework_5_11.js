6cd554f478287ac9ae78b4fe2cb4c0fd013ff6f4
Alina Makovey <93222569+ya-aliina@users.noreply.github.com>
2021-11-22 05:27:52 +0200

    Update homework_5_11.js

--- js/homework_5_11.js
+++ js/homework_5_11.js
@@ -1,1 +1,23 @@
+/*--------------------------------------Задача №11--------------------------------------------
+Напишите функцию get_max( arr ), которая находит максимальное значение в заданном массиве.
+--------------------------------------------------------------------------------------------*/
+function get_max( arr ) {
+    if ( !Array.isArray(arr) ) {
+        console.error('Это не массив!');
+        return null;
+    }
 
+    let result = -Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+        let element = arr[i];
+
+        if (element > result) {
+            result = element;
+        }
+    }
+    return result;
+}
+
+let some_array = [23, 12, 9, 62, 25, 95, 99, 34, 96, 74];
+console.log( get_max(some_array) );
