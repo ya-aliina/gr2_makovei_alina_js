1c64ae650d2d4701a3d049435813e293e5f14765
Alina Makovey <93222569+ya-aliina@users.noreply.github.com>
2021-11-22 05:23:47 +0200

    Update homework_5_10.js

--- js/homework_5_10.js
+++ js/homework_5_10.js
@@ -1,1 +1,22 @@
+/*--------------------------------------Задача №10--------------------------------------------
+Напишите функцию get_random( arr ), которая вернет случайный элемент входящего массива arr.
+--------------------------------------------------------------------------------------------*/
+function get_random( arr ) {
+    if ( !Array.isArray(arr) ) {
+        console.error('Это не массив!');
+        return null;
+    }
 
+    let min = 0;
+    let max = arr.length;
+    let temp = Math.random();
+
+    let random_index = (max - min) * temp;
+    let round_random_index = Math.round(random_index);
+    let random_element = arr[round_random_index];
+
+    return random_element;
+}
+
+let array_random = [31, 60, 17, 42, 38, 65, 41, 99, 15, 22, 7, 5, 100];
+console.log( get_random(array_random) );
