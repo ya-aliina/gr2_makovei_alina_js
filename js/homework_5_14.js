29a043fc54433bde8af4960e970c796eb8c16974
Alina Makovey <93222569+ya-aliina@users.noreply.github.com>
2021-11-22 05:31:34 +0200

    Update homework_5_14.js

--- js/homework_5_14.js
+++ js/homework_5_14.js
@@ -1,1 +1,32 @@
+/*--------------------------------------Задача №14--------------------------------------------
+Создайте функцию is_empty(obj), которая возвращает true, если в объекте нет свойств и false –
+если хоть одно свойство есть.
+Например:
+var schedule = {};
+isEmpty(schedule) ; // должно вернуть true
+schedule["8:30"] = "подъём";
+isEmpty(schedule) ; //должно вернуть false
+--------------------------------------------------------------------------------------------*/
+// 1 вариант решения
 
+// function is_empty(obj) {
+//     for (let key in obj) {
+//         return false;
+//     }
+//     return true;
+// }
+
+// 2 вариант решения
+function is_empty(obj) {
+    if (Object.keys(scedule).length === 0) {
+        console.warn('Данный объект не содержит элементов');
+        return true;
+    }
+    return false;
+}
+
+let scedule = {};
+scedule['8:30'] = 'Подъем';
+scedule['9:00'] = 'Завтрак';
+
+console.log(is_empty(scedule))
