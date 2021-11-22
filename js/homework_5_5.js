fafccf884c809f2599974c0f2cd644946708f664
Alina Makovey <93222569+ya-aliina@users.noreply.github.com>
2021-11-22 05:21:33 +0200

    Update homework_5_5.js

--- js/homework_5_5.js
+++ js/homework_5_5.js
@@ -1,1 +1,32 @@
+/*--------------------------------------Задача №5--------------------------------------------
+Напишите функцию search_string(search, string), которая будет проверять наличие подстроки
+search в строке string.
+Функция должна вернуть булев тип и быть регистронезависимой (использовать метод indexOf)
+--------------------------------------------------------------------------------------------*/
 
+function search_string(search, string) {
+
+    if (string.length === 0 || search.length === 0 ) {
+        alert('Вы ничего не ввели');
+        return null;
+    }
+    search = search.toLowerCase();
+    string = string.toLowerCase();
+    console.log({string})
+
+    let search_index = string.indexOf(search);
+
+    if (search_index >= 0) {
+        alert('Да, такое значение есть в строке');
+        return true;
+    }  else {
+        alert('Нет, такого значения нет в строке')
+        return false;
+    }
+}
+
+let string_3 = prompt('Введите предложение');
+let search = prompt('Введите искомое значение');
+
+let result_5 = search_string(search, string_3);
+console.log(result_5);
