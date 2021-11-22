90649ebb9beb3d6703f40f4d2057df732036474d
Alina Makovey <93222569+ya-aliina@users.noreply.github.com>
2021-11-22 05:22:26 +0200

    Update homework_5_7.js

--- js/homework_5_7.js
+++ js/homework_5_7.js
@@ -1,1 +1,21 @@
+/*--------------------------------------Задача №7--------------------------------------------
+Напишите функцию get_type_file( file_name ), которая получает расширение файла в заданном
+имени файла file_name (пример: get_type_file("readme.txt"), функция должна вернуть ".txt")
+--------------------------------------------------------------------------------------------*/
 
+function get_type_file( file_name ) {
+    if (typeof file_name !== 'string') {
+        console.error('Это не строка!');
+        return null;
+    }
+
+    let new_array = file_name.split('.');
+    let index_last_element = new_array.length - 1;
+    let last_element = new_array[index_last_element];
+
+    let result = `.${last_element}`;
+    return result;
+}
+
+let file_name = 'readme.txt';
+console.log( get_type_file(file_name) );
